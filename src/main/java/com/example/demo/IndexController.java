7c78a738af3a30575d34d7d558cf751df1e760e9
孟德佳 <mengdejia@hyperchain.cn>
2019-08-13 14:40:42 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,15 @@
+//package com.example.demo;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @Author: Dejia Meng
+// * @Date: 2019-08-13 14:25
+// */
+//@RequestMapping("/")
+//@Controller
+//public class IndexController {
+//
+//}
